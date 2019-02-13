d670a635322366e8c0f3336e9fa56da124c1f772
Lisa Lemons - Grand Circus <LisaLemonsGrandCircus@Katies-MacBook-Air.local>
2019-02-13 17:01:24 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Set up variables here
+
+// Set up function setHTMLValentinesName here
+
+
+// Set up function setHTMLValentinesMessage here
+
+
+// Set up function processValentines here
+
+// Call process Valentines here
